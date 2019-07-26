99e61d128f7516f7af9cc74f00ce4083e749c896
GeorgeKariuki7205 <ngugigeorge697@gmail.com>
2019-07-26 06:12:47 +0300

    Added migrations  and models

---
+++ database/migrations/2019_07_26_030727_create_appointment_bookeds_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAppointmentBookedsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appointment_bookeds', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('appointment_bookeds');
+    }
+}
