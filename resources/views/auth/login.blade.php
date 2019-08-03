3b2bf1efe6aacf6c8f38b944439e0df9c2f233a4
GeorgeKariuki7205 <ngugigeorge697@gmail.com>
2019-08-03 14:52:20 +0300

    Made change.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
-@extends('layouts.app')
+@extends('superTemplates\basicTemplate')
 
-@section('content')
-<div class="container">
+@section('body')
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -69,5 +69,50 @@
             </div>
         </div>
     </div>
+</div> --}}
+
+
+
+<div style="background-image:url(&quot;design/assets/img/pexels-photo-433267.jpeg&quot;);">
+    <form id="form" style="font-family:Quicksand, sans-serif;background-color:rgba(34,30,42,0.65);width:320px;padding:40px;" action="{{ route('login') }}" method="post">
+        {{ csrf_field() }}
+        <h2 id="head" style="color:rgb(88,222,55);font-family:Times New Roman;"><strong>LOGIN&nbsp;</strong></h2>
+        <div style="text-align:center;"><img class="rounded-circle img-fluid" src="design/assets/img/download (1).png" id="image" style="width:auto;height:auto;"></div>
+        <div class="form-group"><label for="from-phone" style="font-family:Times New Roman;"><strong>Email :</strong></label><span class="required-input">*</span>
+            <div class="input-group">
+                <div class="input-group-prepend"><span class="input-group-text"><i class="fa fa-user"></i></span></div><input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" placeholder="Email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror</div>
+        </div>
+        <div class="form-group"><label for="from-phone" style="font-family:Times New Roman;"><strong>Password</strong></label><span class="required-input">*</span>
+            <div class="input-group">
+                <div class="input-group-prepend"><span class="input-group-text"><i class="fa fa-key"></i></span></div> <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password" name="password" required autocomplete="current-password">
+
+                @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror</div>
+        </div>
+        
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+            <label class="form-check-label" for="remember" style="color:white">
+                {{ __('Remember Me') }}
+            </label>
+        </div>
+
+        <button class="btn btn-success" type="submit" id="butonas" style="width:100%;height:100%;margin-bottom:10px;background-color:rgb(18,217,14);font-family:Times New Roman;color:rgb(255,255,255);"><strong>LOG IN&nbsp; &nbsp; &nbsp;&nbsp;</strong><i class="fa fa-unlock" style="font-size:32px;"></i></button>
+        @if (Route::has('password.request'))
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        {{ __('Forgot Your Password?') }}
+                                    </a>
+                                @endif
+    </form>
 </div>
 @endsection
